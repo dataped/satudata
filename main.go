f0f831cc8a52565d99c41fd65dbbe117044b0acb
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-10 14:42:15 +0700

    sip

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/dataped/mapdf"
 	"github.com/dataped/satudata/config"
 
 	"github.com/aiteung/musik"
@@ -14,6 +15,7 @@ import (
 )
 
 func main() {
+	mapdf.CreateFolder(config.UploadDir)
 	site := fiber.New(config.Iteung)
 	site.Use(cors.New(config.Cors))
 	url.Web(site)
